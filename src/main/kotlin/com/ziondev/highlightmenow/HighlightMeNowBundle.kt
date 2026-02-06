9bf14ed2bb093348ab644c328cf1fac43a338983
Leonardo K. R. <leo@ziondev.us>
2026-02-06 10:54:08 -0300

    feat: add plugin descriptor and i18n bundle

---
+++ src/main/kotlin/com/ziondev/highlightmenow/HighlightMeNowBundle.kt
@@ -0,0 +1,18 @@
+package com.ziondev.highlightmenow
+
+import com.intellij.AbstractBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+@NonNls
+private const val BUNDLE = "messages.HighlightMeNowBundle"
+
+object HighlightMeNowBundle : AbstractBundle(BUNDLE) {
+    @JvmStatic
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+        getMessage(key, *params)
+
+    @JvmStatic
+    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+        getLazyMessage(key, *params)
+}
